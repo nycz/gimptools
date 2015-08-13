afc7f9b722c228b14dd800cbc07a42dbe298e3e6
nycz <pyntix@gmail.com>
2015-08-13 17:14:35 +0200

    Added merge down layer script
    It finds all layers whose names start with a specific string and merges
    them down with the layer below

---
+++ NyczMergeDownLayers.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2
+
+from gimpfu import *
+
+
+def merge_down_layers(image, layer, name):
+    gimp.pdb.gimp_image_undo_group_start(image)
+    # Make sure there is something to merge
+    while len(image.layers) > 1:
+        for l in image.layers:
+            if l.name.startswith(name):
+                gimp.pdb.gimp_image_merge_down(image, l, NORMAL_MODE)
+                break
+        else:
+            break
+    gimp.pdb.gimp_image_undo_group_end(image)
+    return
+
+register(
+    "nycz_merge_down_layers",
+    "Merge down specific layers",
+    "Merge down layers whose names begins with a specified string",
+    "Nycz",
+    "Nycz",
+    "August 2015",
+    "<Image>/Nycz/Merge down layers...",
+    "RGBA*",
+    [
+        (PF_STRING, "name", "Layers to merge begins with:", "merge")
+    ],
+    [],
+    merge_down_layers,
+    )
+
+main()
